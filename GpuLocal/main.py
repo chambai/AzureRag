cebff9e7fb8768e6c6000032a0ff5443930bf733
chambai <lozzieeinspace@gmail.com>
2026-02-10 16:23:30 +0000

    gpu service port updated

--- GpuLocal/main.py
+++ GpuLocal/main.py
@@ -28,7 +28,7 @@ def get_embeddings(
 
 if __name__ == "__main__":
     from embeddings import embed_text
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
 else:
     # (for unit testing)
     from .embeddings import embed_text
